7794e8d325c15b3a3334512fc1e9696c383268c6
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-11-10 18:31:41 +0300

    Update bucket.tf

--- 23.2_lb/git/bucket.tf
+++ 23.2_lb/git/bucket.tf
@@ -1,9 +1,9 @@
-# Create SA
+# Создание сервисной УЧ
 resource "yandex_iam_service_account" "sa-bucket" {
   name = "sa-backet"
 }
 
-# Grant permissions
+# Выделение прав для сервисной УЧ
 resource "yandex_resourcemanager_cloud_iam_member" "bucket-editor" {
   cloud_id   = var.cloud_id
   role       = "storage.editor"
@@ -11,13 +11,13 @@ resource "yandex_resourcemanager_cloud_iam_member" "bucket-editor" {
   depends_on = [ yandex_iam_service_account.sa-bucket ]
 }
 
-# Create Static Access Keys
+# Создание статического ключа доступа
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.sa-bucket.id
   description        = "static access key for bucket"
 }
 
-# Use keys to create bucket
+# Использование ключа для бакета
 resource "yandex_storage_bucket" "netology-bucket" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
@@ -25,7 +25,7 @@ resource "yandex_storage_bucket" "netology-bucket" {
   acl        = "public-read"
 }
 
-# Add picture to bucket
+# Добавляем графический файл в бакет
 resource "yandex_storage_object" "object-1" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
